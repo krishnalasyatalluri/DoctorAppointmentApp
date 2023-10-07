6246eb450b0bd66beb0dab6f41c9b96c6d6a84dc
Krishna <krishnalasyatalluri9@gmail.com>
2023-10-07 18:14:34 +0530

    added all models

---
+++ backend/models/bookingModel.js
@@ -0,0 +1,43 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+const bookingSchema=new Schema({
+    date:{
+        type:Date,
+        required:true
+    },
+    patientId:{
+        type:Schema.Types.patientId,
+        ref:'Patient',
+        required:true
+
+    },
+    doctorId:{
+        type:Schema.Types.doctorId,
+        ref:'Doctor',
+        required:true
+
+    },
+    totalAmount:{
+        type:Number,
+        required:[true,'Amount is required']
+    },
+    paymentMode:{
+        type:String,
+        required:true
+    },
+    paymentStatus:{
+        type:Boolean,
+        required:true
+    },
+    
+
+    bookingCompletion:{
+        type:Number,
+        required:[true,'experience is required']
+    }
+
+
+})
+const Booking=mongoose.model('Booking',bookingSchema)
+module.exports = Booking
+
---
+++ backend/models/slotModel.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+const slotSchema=new Schema({
+    doctorId:{
+        type:Schema.Types.doctorId,
+        required:true,
+        ref:'Doctor'
+       
+    },
+    startDateTime:{
+        type:Date,
+        required:true
+    },
+    endDateTime:{
+        type:Date,
+        required:true
+    },
+    isAvailable:{
+        type:Boolean,
+        required:true
+    }
+    
+
+
+})
+const Slot=mongoose.model('Slot',slotSchema)
+module.exports = Slot
+
